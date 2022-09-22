ccaf3fced5bd1b2c008a9d7b98c211dc7e113add
Thauan Felipe <106314051+ThauanFelipe98@users.noreply.github.com>
2022-09-22 19:34:36 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function insert(num){
+    var numero = document.getElementById("resultado").innerHTML;
+   document.getElementById("resultado").innerHTML = numero + num;
+
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back(){
+    var res =document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = res.substring(0, res.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById("resultado").innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "Error!!!";
+    }
+}
